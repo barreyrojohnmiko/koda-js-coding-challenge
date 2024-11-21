0ab2a5bfc54a8fcb9caef916afd2cf9f717bdda1
Miko Barreyro <110441796+barreyrojohnmiko@users.noreply.github.com>
2024-11-21 16:40:03 +0800

    changed web app name to Koda web

--- web-app/src/pages/result/page.tsx
+++ web-app/src/pages/result/page.tsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import ResultListView from '../../views/resultList/ResultList.tsx';
+
 import { loadQuizData } from '../../services/AppService.tsx';
 
 export default function ResultPage() {
