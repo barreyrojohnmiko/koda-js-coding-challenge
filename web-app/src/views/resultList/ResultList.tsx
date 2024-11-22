58f15e59463a05a50c321ecd161a31fd19c355f2
Miko Barreyro <110441796+barreyrojohnmiko@users.noreply.github.com>
2024-11-22 16:00:35 +0800

    added padding

--- web-app/src/views/resultList/ResultList.tsx
+++ web-app/src/views/resultList/ResultList.tsx
@@ -1,7 +1,7 @@
 import './styles.scss';
 
 import { Box, Typography } from '@mui/material';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { useSelector } from 'react-redux';
 
